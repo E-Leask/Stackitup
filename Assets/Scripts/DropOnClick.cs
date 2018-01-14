199d76f6a654faa79c0b5f1fcba38a1f48c60fc2
Eric.Leask <16efl@queensu.ca>
2018-01-14 15:50:51 -0500

    Not Working
    Cant count time in outside of update
    I do not know dae wae

---
+++ Assets/Scripts/DropOnClick.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropOnClick : MonoBehaviour {
+	public Rigidbody2D rb2D;
+	private float maxWidth;
+	private bool mouseClicked=false;
+	private float lastClick=1;
+	public Camera cam;
+	public DropOnClick(){
+	}
+	// Use this for initialization
+	void Start () {
+			if (cam==null)
+			{
+				cam = Camera.main;
+			}
+		Vector3 upperCorner = new Vector3 (Screen.width, Screen.height, 0.0f);
+		Vector3 targW = cam.ScreenToWorldPoint (upperCorner);
+		maxWidth = targW.x;
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		lastClick -= Time.deltaTime;
+		if (!mouseClicked) {
+			Vector3 rawPos = cam.ScreenToWorldPoint (Input.mousePosition);
+			Vector3 targPos = new Vector3 (rawPos.x, transform.position.y, 0.0f);
+			float targW = Mathf.Clamp (targPos.x, -maxWidth, maxWidth);
+			targPos = new Vector3 (targW, targPos.y, targPos.z);
+			rb2D.MovePosition (targPos);
+		}
+		if (Input.GetMouseButtonDown(0)) {//And if 1 second since the last click
+			mouseClicked = true;
+			rb2D.bodyType = RigidbodyType2D.Dynamic;
+	}
+}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,7 +6,9 @@ public class GameController : MonoBehaviour {
 	public Camera cam;
 	private float maxWidth;
 	private bool camControl;
-	public GameObject dropItem;
+	public GameObject Square;
+	public GameObject Triangle;
+	private float lastClick=1;
 
 	// Use this for initialization
 	void Start () {
@@ -20,24 +22,30 @@ public class GameController : MonoBehaviour {
 	}
 
 	IEnumerator SpawnItem(GameObject item){
+		lastClick = 1;
+		int countdown = 100;
+		float wait = Time.timeSinceLevelLoad;
+		while (countdown > 0) {
+			print(" Time since Load;"+Time.timeSinceLevelLoad+ "  Time time"+Time.time+"  Time delta"+Time.deltaTime);
 
+			--countdown;
+		}
 		Vector3 rawPos = cam.ScreenToWorldPoint (Input.mousePosition);
 		Vector3 targPos = new Vector3 (rawPos.x, transform.position.y , 0.0f);
 		float targW = Mathf.Clamp (targPos.x, -maxWidth, maxWidth);
 		targPos = new Vector3(targW, targPos.y, targPos.z);
-	Instantiate(item, targPos, Quaternion.Euler(new Vector3(0, 0, 0)));
-		yield return new WaitForSeconds (5);
+		Instantiate(item, targPos, Quaternion.Euler(new Vector3(0, 0, 0)));
+		yield return new WaitForSeconds (1);
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		//yield return new WaitForSeconds (2);
-		if (Input.GetMouseButtonDown (0)) {
-			StartCoroutine(SpawnItem(dropItem));
+		lastClick -= Time.deltaTime;
+		if (Input.GetMouseButtonDown (0)&& lastClick<0) {
+			StartCoroutine(Invoke(SpawnItem(Square),2f));
 		}
-		//on mouse click release block
-		//wait 2 seconds
-		//spawn new block at location of gamecontroller
+		//spawn new block at x location of mouse and follow
 	}
 }
