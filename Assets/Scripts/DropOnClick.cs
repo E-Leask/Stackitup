d514f65f1d29271f7ce4af559ed2316adc6f0a7b
Eric.Leask <16efl@queensu.ca>
2018-01-14 17:40:18 -0500

    May have found the way
    disable colliders when spawned initially but they won't spawn :((((

--- Assets/Scripts/DropOnClick.cs
+++ Assets/Scripts/DropOnClick.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DropOnClick : MonoBehaviour {
 	public Rigidbody2D rb2D;
+	public Collider2D cldr2D;
 	private float maxWidth;
 	private bool mouseClicked=false;
 	private float lastClick=1;
@@ -16,6 +17,7 @@ public class DropOnClick : MonoBehaviour {
 			{
 				cam = Camera.main;
 			}
+		cldr2D = GetComponent<Collider2D>();
 		Vector3 upperCorner = new Vector3 (Screen.width, Screen.height, 0.0f);
 		Vector3 targW = cam.ScreenToWorldPoint (upperCorner);
 		maxWidth = targW.x;
@@ -24,7 +26,7 @@ public class DropOnClick : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		lastClick -= Time.deltaTime;
+		//lastClick -= Time.deltaTime;
 		if (!mouseClicked) {
 			Vector3 rawPos = cam.ScreenToWorldPoint (Input.mousePosition);
 			Vector3 targPos = new Vector3 (rawPos.x, transform.position.y, 0.0f);
@@ -35,6 +37,7 @@ public class DropOnClick : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0)) {//And if 1 second since the last click
 			mouseClicked = true;
 			rb2D.bodyType = RigidbodyType2D.Dynamic;
+			cldr2D.enabled = true;
 	}
 }
 }
